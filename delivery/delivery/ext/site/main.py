c8d57410c42e60de0dcacb06aaf5e332ed54f4fc
Luciano Martins <zicadopv@gmail.com>
2020-07-05 22:10:03 -0300

    Implementando templates e flask-debugtoolbarestension

--- delivery/delivery/ext/site/main.py
+++ delivery/delivery/ext/site/main.py
@@ -6,8 +6,13 @@ bp = Blueprint("site", __name__)
 
 @bp.route("/")
 def index():
+    return render_template("index.html")
 
-    return render_template(
-        "index.html",
-        name=request.args['name']
-    )
+
+@bp.route("/sobre")
+def about():
+    return render_template("about.html")
+
+@bp.route("/restaurantes")
+def restaurants():
+    return render_template("restaurants.html")
